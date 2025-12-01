ed4c43e000fc8a58a2155e269f9e2665a08cd20f
Falah Hilal <falahhilal2018@gmail.com>
2025-12-01 23:23:04 +0500

    fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
-/*const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: NextConfig = {
+   /*config options here */
 };
 
-//export default nextConfig; */
-
-// next.config.js
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
 export default nextConfig; 
