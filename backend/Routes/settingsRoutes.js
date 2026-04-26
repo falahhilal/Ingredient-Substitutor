bfbd6bd0473b1a75ffa3c1a57cf428e11916a8b3
Falah Hilal <falahhilal2018@gmail.com>
2026-04-27 00:19:25 +0500

    backeend fix

--- backend/Routes/settingsRoutes.js
+++ backend/Routes/settingsRoutes.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 const router = express.Router();
 const settingsController = require('../Controllers/settingsController');
 
-router.post('/', saveUserPreferences);
-router.get('/', getUserPreferences);
+router.post('/', settingsController.saveUserPreferences);
+router.get('/', settingsController.getUserPreferences);
 router.post('/change-password', settingsController.changePassword);
 
 module.exports = router;
